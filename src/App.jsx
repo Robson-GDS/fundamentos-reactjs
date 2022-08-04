0b3a9861cdbf517ea6b37de3d3cd15013f9a228f
unknown <robson.080490@gmail.com>
2022-08-04 07:57:39 -0300

    criando estilo da pagina principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
-import styles from './app.module.css';
+import styles from './App.module.css';
 
 import './global.css';
 
