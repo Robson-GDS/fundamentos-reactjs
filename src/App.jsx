570aa876ca6798082ec3e2c597924da889d105a7
unknown <robson.080490@gmail.com>
2022-08-04 08:19:54 -0300

    estruturando aside

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
-import styles from './App.module.css';
+import styles from "./App.module.css";
 
-import './global.css';
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
@@ -11,9 +12,7 @@ export function App() {
       <Header />
       
       <div className={styles.wrapper}>
-        <aside>
-          sidebar
-        </aside>
+        <Sidebar />
         <main>
           <Post 
             author="Robson Gomes" 
