aac370c4125415c5750c24307aad245d12572acb
unknown <robson.080490@gmail.com>
2022-08-11 11:34:17 -0300

    criando estrutura dos comentarios

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { Trash, ThumbsUp } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/robson-gds.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Robson Gomes</strong>
+              <time title="05 de agosto às 08:13h" dateTime="2022/08/05 08:13:30">Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom!</p>
+        </div>
+
+        <footer> 
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
