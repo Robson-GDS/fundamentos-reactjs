21e36c87eab39e23bb77b8397b17752fd51e80da
unknown <robson.080490@gmail.com>
2022-08-19 06:58:09 -0300

    iterando JSX

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,41 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/robson-gds.png",
+      name: "Robson Gomes",
+      role: "Web Developer"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera", },
+
+      { type: "paragraph", content: "consectetur adipiscing elit. Maecenas non ex nisl. Ut vel finibus diam, nec sodales justo.", },
+
+      { type: "link", content: "neque.iaculis/dictum" }
+    ],
+    publishedAt: new Date("2022-08-18 20:00:00")
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/robson-gds.png",
+      name: "Robson Gomes",
+      role: "Web Developer"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera", },
+
+      { type: "paragraph", content: "consectetur adipiscing elit. Maecenas non ex nisl. Ut vel finibus diam, nec sodales justo.", },
+
+      { type: "link", content: "neque.iaculis/dictum" }
+    ],
+    publishedAt: new Date("2022-08-19 20:00:00")
+  },
+]
+
 export function App() {
   return (
     <>
@@ -14,14 +49,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Robson Gomes" 
-            content="Nulla ornare tempus enim, eu vestibulum ligula faucibus vel. Vivamus et velit ut nunc bibendum sagittis sed eu dolor."
-          />
-          <Post 
-            author="Cheetos Kid" 
-            content="Nulla ornare tempus enim, eu vestibulum ligula faucibus vel. Vivamus et velit ut nunc bibendum sagittis sed eu dolor."
-          />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </>
