ded6ae4c8ebf6eaa83fa68fb27f0b52c20832f99
unknown <robson.080490@gmail.com>
2022-08-22 06:21:19 -0300

    criando componente avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
 import styles from "./Sidebar.module.css";
 
@@ -11,7 +12,8 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/robson-gds.png"/>
+        <Avatar src="https://github.com/robson-gds.png" />
+
         <strong>Robson Gomes</strong>
         <span>Web Developer</span>
       </div>
