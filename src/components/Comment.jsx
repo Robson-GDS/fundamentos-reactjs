a639579042ffea124eac58102e1ae9abf8357579
unknown <robson.080490@gmail.com>
2022-09-26 14:11:37 -0300

    Inserindo comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,7 +4,7 @@ import { Avatar } from "./Avatar";
 
 import styles from "./Comment.module.css";
 
-export function Comment() {
+export function Comment({ content }) {
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/robson-gds.png" />
@@ -22,7 +22,7 @@ export function Comment() {
             </button>
           </header>
 
-          <p>Muito bom!</p>
+          <p>{content}</p>
         </div>
 
         <footer> 
